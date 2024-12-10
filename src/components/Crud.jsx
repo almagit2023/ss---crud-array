6bb60458e8871431976c71188204eb3e77b004c7
almacode1703 <testmygitt.2023@gmail.com>
2024-12-10 13:08:22 +0530

    Updated Crud.jsx

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -4,7 +4,9 @@ import './Crud.css'
 const Crud = () => {
 
   const [name, setName] = useState([]);
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
+  const [editingId, setEditingId] = useState(null);
+  const [editedName, setEditedName] = useState("")
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -24,8 +26,17 @@ const Crud = () => {
     setName(newName);
   }
 
-  const handleEdit = () => {
+  const handleEdit = (namedIdx) => {
+    const updatedNames = [...name].map((item) => {
+      if (item.id === namedIdx) {
+        item.name = editedName
+      }
+      return item;
+    })
 
+    setName(updatedNames);
+    setEditedName("");
+    setEditingId(null)
   }
 
   return (
@@ -34,22 +45,34 @@ const Crud = () => {
         <label htmlFor="name">Enter your name </label>
         <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
         <button onClick={handleClick}>Submit</button>
-
       </form>
       {
         name.map((nameData, idx) => {
           return (
             <div className='resultData'>
-              <li>{nameData.name}</li>
+
+              {editingId === nameData.id ?
+                (<input type="text" value={editedName} onChange={(e) => setEditedName(e.target.value)} />)
+                :
+                (<li>{nameData.name}</li>)
+              }
+
               <div className='btnGroup'>
+
                 <button onClick={(e) => handleDelete(e, nameData.id)}>Delete</button>
-                <button onClick={(e) => handleEdit(e, nameData.id)}>Edit</button>
+                {
+                  editingId === nameData.id ?
+                    (<button onClick={() => handleEdit(nameData.id)}>Submit Edit</button>)
+                    :
+                    (<button onClick={() => { setEditingId(nameData.id); setEditedName(nameData.name); }}>Edit</button>)
+                }
               </div>
+
             </div>
           )
         })
       }
-    </div>
+    </div >
   )
 }
 
